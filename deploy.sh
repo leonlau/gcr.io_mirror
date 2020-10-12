7a4903e9c166f243fc676373b51724930af57ff0
leonlau <386003863@qq.com>
2020-10-12 16:56:38 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -27,3 +27,59 @@ imgdsc='lauleon/jenkinsxio-nginx-ingress-controller:0.30.0'
 docker  pull ${imgsrc}
 docker tag  ${imgsrc} ${imgdsc}
 docker push ${imgdsc}
+
+
+
+imgsrc="k8s.gcr.io/kube-apiserver:v1.18.9"
+imgdsc='lauleon/kube-apiserver:v1.18.9'
+docker  pull ${imgsrc}
+docker tag  ${imgsrc} ${imgdsc}
+docker push ${imgdsc}
+
+
+imgsrc="k8s.gcr.io/kube-controller-manager:v1.18.9"
+imgdsc='lauleon/kube-controller-manager:v1.18.9'
+docker  pull ${imgsrc}
+docker tag  ${imgsrc} ${imgdsc}
+docker push ${imgdsc}
+
+
+imgsrc="k8s.gcr.io/kube-scheduler:v1.18.9"
+imgdsc='lauleon/kube-scheduler:v1.18.9'
+docker  pull ${imgsrc}
+docker tag  ${imgsrc} ${imgdsc}
+docker push ${imgdsc}
+
+
+
+
+imgsrc="k8s.gcr.io/kube-proxy:v1.18.9"
+imgdsc='lauleon/kube-proxy:v1.18.9'
+docker  pull ${imgsrc}
+docker tag  ${imgsrc} ${imgdsc}
+docker push ${imgdsc}
+
+
+
+
+imgsrc="k8s.gcr.io/pause:3.2"
+imgdsc='lauleon/pause:3.2'
+docker  pull ${imgsrc}
+docker tag  ${imgsrc} ${imgdsc}
+docker push ${imgdsc}
+
+
+
+imgsrc="k8s.gcr.io/etcd:3.4.3-0"
+imgdsc='lauleon/etcd:3.4.3-0'
+docker  pull ${imgsrc}
+docker tag  ${imgsrc} ${imgdsc}
+docker push ${imgdsc}
+
+
+imgsrc="k8s.gcr.io/coredns:1.6.7"
+imgdsc='lauleon/coredns:1.6.7'
+docker  pull ${imgsrc}
+docker tag  ${imgsrc} ${imgdsc}
+docker push ${imgdsc}
+
