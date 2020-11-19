bf2469e5ce433d705285764bd45a13c6e2537dfd
leonlau <386003863@qq.com>
2020-11-19 16:28:27 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,64 +1,21 @@
 #!/usr/bin/env bash
 
 
-imgsrc="gcr.io/linkerd-io/controller:stable-2.8.1"
-imgdsc='lauleon/linkerd-io-controller:stable-2.8.1'
+imgsrc="gcr.io/tekton-releases/github.com/tektoncd/pipeline/cmd/controller:v0.18.1@sha256:0d1dcd40d032e940da112460dfbff8da9a423cfcb2397388f2703548b7d406ef"
+imgdsc='lauleon/tektoncd-controller:v0.18.1'
 docker  pull ${imgsrc}
 docker tag  ${imgsrc} ${imgdsc}
 docker push ${imgdsc}
 
-
-imgsrc="gcr.io/linkerd-io/proxy:stable-2.8.1"
-imgdsc='lauleon/linkerd-io-proxy:stable-2.8.1'
+imgsrc="gcr.io/tekton-releases/github.com/tektoncd/pipeline/cmd/entrypoint:v0.18.1@sha256:59d56863e61d9b17f4d1529eecaf081e8ad08926bd008fb77e86ca4c2d9ef7d6"
+imgdsc='lauleon/tektoncd-entrypoint:v0.18.1'
 docker  pull ${imgsrc}
 docker tag  ${imgsrc} ${imgdsc}
 docker push ${imgdsc}
 
-imgsrc="gcr.io/linkerd-io/proxy-init:v1.3.3"
-imgdsc='lauleon/linkerd-io-proxy-init:v1.3.3'
+imgsrc="gcr.io/tekton-releases/github.com/tektoncd/pipeline/cmd/webhook:v0.18.1@sha256:96cebd8a17eb204aee86a4ec8cd576fd0d2bbdefc74bdfe17d46b2bdf0bc5e6c"
+imgdsc='lauleon/tektoncd-webhook:v0.18.1'
 docker  pull ${imgsrc}
-docker tag  ${imgsrc} ${imgdsc}
-docker push ${imgdsc}
-
-imgsrc="gcr.io/linkerd-io/grafana:stable-2.8.1"
-imgdsc='lauleon/linkerd-io-grafana:stable-2.8.1'
-docker  pull ${imgsrc}
-docker tag  ${imgsrc} ${imgdsc}
-docker push ${imgdsc}
-
-imgsrc="gcr.io/linkerd-io/web:stable-2.8.1"
-imgdsc='lauleon/linkerd-io-web:stable-2.8.1'
-docker  pull ${imgsrc}
-docker tag  ${imgsrc} ${imgdsc}
-docker push ${imgdsc}
-
-
-
-imgsrc="gcr.io/tekton-releases/github.com/tektoncd/pipeline/cmd/imagedigestexporter:v0.16.3"
-imgdsc='lauleon/tektoncd-pipeline-imagedigestexporter:v0.16.3'
-docker  pull ${imgsrc}
-docker tag  ${imgsrc} ${imgdsc}
-docker push ${imgdsc}
-
-
-
-
-imgsrc="gcr.io/tekton-releases/github.com/tektoncd/pipeline/cmd/controller:v0.16.3"
-imgdsc='lauleon/tektoncd-pipeline-controller:v0.16.3'
-docker  pull ${imgsrc}
-docker tag  ${imgsrc} ${imgdsc}
-docker push ${imgdsc}
-
-
-imgsrc="gcr.io/tekton-releases/github.com/tektoncd/pipeline/cmd/webhook:v0.16.3"
-imgdsc='lauleon/tektoncd-pipeline-webhook:v0.16.3'
-docker pull ${imgsrc}
-docker tag  ${imgsrc} ${imgdsc}
-docker push ${imgdsc}
-
-imgsrc="gcr.io/spark-operator/spark:v3.0.0"
-imgdsc='lauleon/spark-operator-spark:v3.0.0'
-docker pull ${imgsrc}
 docker tag  ${imgsrc} ${imgdsc}
 docker push ${imgdsc}
 
